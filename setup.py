a61a808e3ac5937eba50a82eccdf9589f45ace52
Aniket <iamaniketkumarmaner@gmail.com>
2026-02-14 17:36:46 +0530

    Add main module to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="minichain",
     version="0.1.0",
     packages=find_packages(),  # Will detect core, consensus, network, etc.
+    py_modules=["main"],
     install_requires=[
         "pynacl",
         "py-libp2p",
